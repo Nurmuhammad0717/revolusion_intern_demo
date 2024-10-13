1a0229e4f8d6810504fc32d37dfc49c9d8ad8275
nurmukhammadsobirov <sobirovn0717@gmail.com>
2024-10-13 13:02:00 +0500

    first commit

---
+++ src/main/java/uz/pdp/revolusion_intern_demo/RevolusionInternDemoApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.revolusion_intern_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevolusionInternDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevolusionInternDemoApplication.class, args);
+	}
+
+}
